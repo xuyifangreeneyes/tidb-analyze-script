aba15c2366ca9b3cfcb0104738cb4a88839f1c30
xuyifan <675434007@qq.com>
2022-11-12 16:30:17 +0800

    fix

--- tidb_analyze_tables.sh
+++ tidb_analyze_tables.sh
@@ -4,7 +4,7 @@ tidb_host="127.0.0.1"
 tidb_port=4000
 tidb_user="root"
 
-healthy_threshold=0.5
+healthy_threshold=50
 tidb_build_stats_concurrency=1
 tidb_distsql_scan_concurrency=1
 analyze_max_execution_time=0
@@ -29,14 +29,14 @@ do
                 fi
             else
                 if [[ $(echo "${row[3]} < $healthy_threshold" | bc) -eq 1 ]]; then
-                    need_analyze=1 
+                    need_analyze=1
                     if [ "${row[2]}" = "global" ]; then
                         analyze_global=1
                     else
                         if [[ -n "$partitions" ]]; then
-                            partitions="$partitions" ",${row[2]}"
+                            partitions="$partitions,${row[2]}"
                         else
-                            partitions="\`${row[2]}\`"
+                            partitions="${row[2]}"
                         fi
                     fi
                 fi
@@ -47,13 +47,13 @@ do
             continue
         fi
         is_analyzing=$(mysql -h$tidb_host -P$tidb_port -u$tidb_user -D$schema  -Nse "select count(1) from mysql.analyze_jobs where table_schema = \"$schema\" and table_name = \"$table\" and state in ('pending', 'running') and update_time > current_timestamp() - interval 6 hour")
-        if [[ ${{is_analyzing}} -gt 0 ]]; then
+        if [[ is_analyzing -gt 0 ]]; then
             echo "table $schema.$table is being analyzed, skip"
             continue
         fi
-        analyze_command="setanalyze table \`$schema\`.\`$table\`"
-        if [[ -n "$partition" ]] && [[ analyze_global -eq 0 ]]; then
-            analyze_command="$analyze_command" " partition $partitions"
+        analyze_command="analyze table $schema.$table"
+        if [[ -n "$partitions" ]] && [[ analyze_global -eq 0 ]]; then
+            analyze_command="$analyze_command partition $partitions"
         fi
         echo "`date` | $analyze_command start"
         mysql -h$tidb_host -P$tidb_port -u$tidb_user -D$schema -e "set @@session.tidb_build_stats_concurrency=$tidb_build_stats_concurrency;set @@session.tidb_distsql_scan_concurrency=$tidb_distsql_scan_concurrency;set @@session.max_execution_time=$analyze_max_execution_time;$analyze_command" 
